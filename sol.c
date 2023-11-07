56500b51291d224e958c9f7f079de982ee54bb3c
tubond <tybond2003@ya.ru>
2023-11-07 23:53:14 +0300

    v2

--- sol.c
+++ sol.c
@@ -3,13 +3,65 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <dlfcn.h>
+#include <string.h>
 
 static RandomSource *destroy(RandomSource *src);
 static double next(RandomSource *src);
 
+const char name_l[] = "linear";
+const char name_r[] = "random";
+
+enum {
+    ARGC = 4,
+    FOO_NAME = 1,
+    FOO_SIZE = 2,
+    FOO_ARG = 3,
+    BASE = 10,
+    MAX_SIZE = 100000
+};
+
 int
 main(int argc, char **argv)
 {
-    
+    if (argc != ARGC) {
+        fprintf(stderr, "Input error\n");
+        exit(1);
+    }
+
+    errno = 0;
+    char *eptr = NULL;
+    long count = strtol(argv[FOO_SIZE], &eptr, BASE);
+    if (errno || *eptr || eptr == argv[FOO_SIZE] || count < 0 || count > MAX_SIZE) {
+        fprintf(stderr, "Input err\n");
+        exit(1);
+    }
+
+    RandomSource *rr;
+
+    void *handle = dlopen(NULL, RTLD_LAZY);
+    if (handle == NULL) {
+        fprintf(stderr, "dlopen err\n");
+        exit(1);
+    }
+
+
+    void *sym;
+    if (strncmp(name_l, argv[FOO_NAME], sizeof(name_l) / sizeof(name_l[0])) == 0) {
+        sym = dlsym(handle, "random_linear_factory");
+        if (sym == NULL) {
+            fprintf(stderr, "dlsym err\n");
+            exit(1);
+        }
+        rr = ((RandomSource *(*) (const char *)) sym)(argv[FOO_ARG]);
+    } else {
+        fprintf(stderr, "STR ERR\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < count; i++) {
+        printf("%f\n", rr->ops->next(rr));
+    }
+    rr->ops->destroy(rr);
+
     return 0;
 }
